4ea652f563d774cfb418c1ef9909cd1897dfaae0
RPS <rps.rajnish@gmail.com>
2022-10-02 23:40:52 +0530

    h1 bold

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,9 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-black md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-8 mb-6 flex justify-between dark:text-white">
-            <h1 className="font-poppins text-xl tracking-widest ">RPS</h1>
+            <h1 className="font-poppins font-bold text-2xl tracking-widest ">
+              RPS
+            </h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
