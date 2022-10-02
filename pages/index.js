dc35e879f106abe77bf10b43c99cfb9215c8d7af
RPS <rps.rajnish@gmail.com>
2022-10-02 23:58:15 +0530

    padding in h2

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-black md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-8 mb-6 flex justify-between dark:text-white">
-            <h1 className="font-poppins font-bold text-2xl tracking-widest ">
-              RPS
-            </h1>
+            <h1 className="font-poppins font-bold text-2xl  ">R P S</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -46,7 +44,7 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 py-5">
-            <h2 className="text-4xl py-4 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+            <h2 className="text-4xl px-10 py-4 text-teal-600 font-medium dark:text-teal-400  uppercase md:text-6xl">
               Rajnish Pratap Singh
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
