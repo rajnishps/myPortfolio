55ed329eb00d972a144af810f88e1b7760f5665d
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2022-10-14 19:18:56 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
+    <meta name="google-site-verification" content="smlYPaCoj2jlkXd_U03v8YTLonbYC4a_r-FW5k1aMYQ" />
       <Head />
       <body>
         <Main />
